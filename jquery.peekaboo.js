79658a2e3b84d52e27113b2347f92e929a1c98ab
Nadav Spiegelman <me@nadav.name>
2015-10-02 11:59:33 -0400

    Update with ES6 and new options
    - Add destruct option to remove event listener
    - Add namespace option for event listener
    - Add buttonClassname option

--- jquery.peekaboo.js
+++ jquery.peekaboo.js
@@ -1,48 +1,83 @@
-;(function($) {
-    'use strict';
+(function($) {
 
-    $.fn.peekaboo = function() {
-        return this.each(function() {
-            var $this = $(this);
-            var $toggle = $this;
-            var $next;
+  $.fn.peekaboo = function(options) {
+    const config = $.extend({
+      namespace: 'peekaboo',
+      buttonClassname: 'button--transparent',
+      callback: () => {},
+      destruct: false
+    }, options);
 
-            // Use the data attribute if specified
-            // Otherwise, toggle the next element
-            if ($this.data('toggle-target')) {
-                $next = $('#' + $this.data('toggle-target'));
-            } else {
-                $next = $this.next();
-            }
+    function toggleAttr($element, attribute) {
+      const currentValue = $element.attr(attribute) === 'true' || $element.attr(attribute) === 'hidden';
+      $element.attr(attribute, !currentValue);
+    }
 
-            var toggleAttr = function($element, attribute) {
-                var currentValue = $element.attr(attribute) === 'true';
-                $element.attr(attribute, !currentValue);
-            };
+    return this.each(function() {
+      const $this = $(this);
+      let $toggle = $this;
+      let $next;
 
-            // Generate an ID for the content if there isn't one
-            if (!$next.attr('id')) {
-                $next.attr('id', 'peekaboo-' + Math.random().toString(36).substr(2, 9));
-            }
+      // Use the data attribute if specified
+      // Otherwise, toggle the next element
+      if ($this.data('toggle-target')) {
+        $next = $('#' + $this.data('toggle-target'));
+      } else {
+        $next = $this.next();
+      }
 
-            // If the toggle isn't not a button, wrap its contents in a <button>
-            // Better than adding button role because we don't lose the element's semantics
-            if (this.nodeName.toLowerCase() !== 'button') {
-                $this.wrapInner('<button type="button" aria-expanded="false" aria-controls="' + $next.attr('id') + '">');
-                $toggle = $this.find('button');
-            } else {
-                $this.attr('aria-expanded', false).attr('aria-controls', $next.attr('id'));
-            }
+      // Pass { destruct: true } in order to remove the click event for an element
+      if (config.destruct) {
+        let shouldUnwrap = false;
 
-            // Hide the content
-            $next.attr('aria-hidden', true);
+        if (this.nodeName.toLowerCase() !== 'button') {
+          $toggle = $this.find('button');
+          shouldUnwrap = true;
+        }
 
-            $toggle.on('click', function() {
-                toggleAttr($toggle, 'aria-expanded');
-                toggleAttr($next, 'aria-hidden');
-            });
+        $toggle.off(`click.${config.namespace}`).attr('aria-expanded', null);
+        $next.attr('hidden', null);
 
-        });
-    };
+        if (shouldUnwrap) {
+          $toggle.children().unwrap();
+        }
+
+        return;
+      }
+
+      // Use existing aria-expanded value to allow expanded by default
+      let isExpanded = !!$this.attr('aria-expanded');
+      if (window.location.hash == '#' + $this.attr('id')) {
+        isExpanded = true;
+      }
+
+      // Generate an ID for the content if there isn't one
+      if (!$next.attr('id')) {
+        $next.attr('id',
+                   `${config.namespace}-${Math.random().toString(36).substr(2, 9)}`);
+      }
+
+      // If the toggle isn't a button, wrap it in a <button>
+      // Better than adding button role because we don't lose the element's semantics
+      if (this.nodeName.toLowerCase() !== 'button') {
+        const { buttonClassname } = config;
+
+        $this.wrapInner(`<button type="button" class="${buttonClassname}" aria-expanded="${isExpanded}" aria-controls="${$next.attr('id')}">`);
+        $toggle = $this.find('button');
+      } else {
+        $this.attr('aria-expanded', isExpanded).attr('aria-controls', $next.attr('id'));
+      }
+
+      // Hide the content
+      $next.attr('hidden', !isExpanded);
+
+      $toggle.on(`click.${config.namespace}`, function(event) {
+        toggleAttr($toggle, 'aria-expanded');
+        toggleAttr($next, 'hidden');
+        config.callback.call(this, { event });
+      });
+
+    });
+  };
 
 }(window.jQuery));
